e2ab7fd0239936216c8080fe46724ac4b2a925e1
BuildTools <ssnazy767@gmail.com>
2024-11-28 11:43:48 +0100

    added docker integration and changed the code to work properly with sql later on.

--- src/main/java/com/example/demo/Task.java
+++ src/main/java/com/example/demo/Task.java
@@ -1,5 +1,43 @@
 package com.example.demo;
 
-public record Task(long id, String title, String description) {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
 
+@Entity
+@Table(name = "task")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String title;
+
+    private String description;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- src/main/java/com/example/demo/TaskController.java
+++ src/main/java/com/example/demo/TaskController.java
@@ -1,19 +1,20 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.Date;
 import java.util.Map;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@RestController
+@Controller
 public class TaskController {
-    private static Task[] taskList = { new Task(1, "hard", "bone"), new Task(2, "hard", "bone") };
+    @Autowired
+    private TaskList taskList;
 
     @GetMapping("/test")
     public String testerFunction() {
@@ -21,8 +22,20 @@ public class TaskController {
     }
 
     @GetMapping("/tasks")
-    public Task[] getTasks() {
-        return taskList;
+    public @ResponseBody Iterable<Task> getTasks() {
+        // Statement st = con.createStatement();
+        return taskList.findAll();
+    }
+
+    @PostMapping("/tasks")
+    public @ResponseBody String addTask(@RequestParam Map<String, String> params) {
+        String title = params.get("title");
+        String description = params.get("description");
+        Task newTask = new Task();
+        newTask.setTitle(title);
+        newTask.setDescription(description);
+        taskList.save(newTask);
+        return "Saved";
     }
 
     @GetMapping("/tasks/list")
@@ -30,48 +43,27 @@ public class TaskController {
         System.out.println(taskList.toString());
     }
 
-    @GetMapping("/tasks/{id}")
-    public Task getTaskById(@PathVariable("id") Long id) {
-        Task chosenTask = null;
-        for (Task task : taskList) {
-            if (task.id() == id) {
-                chosenTask = task;
-                break;
-            }
-        }
-        return chosenTask;
-    }
+    // @GetMapping("/tasks/{id}")
+    // public Task getTaskById(@PathVariable("id") Long id) {
+    // Task chosenTask = null;
+    // }
+    // }
+    // return chosenTask;
+    // }
 
-    @PostMapping("/tasks")
-    public Task[] addTask(@RequestParam Map<String, String> params) {
-        String title = params.get("title");
-        String description = params.get("description");
-        Long id = new Date().getTime();
-        Task newTask = new Task(id, title, description);
-        Task[] newTaskList = new Task[taskList.length + 1];
-        for (int i = 0; i < taskList.length; i++) {
-            newTaskList[i] = taskList[i];
-        }
-        newTaskList[taskList.length] = newTask;
-        taskList = newTaskList;
-        return newTaskList;
-    }
+    // @DeleteMapping("/tasks/{id}")
+    // public TaskList removeTask(@PathVariable("id") Long id) {
+    // int currentIndex = 0;
+    // Task[] newTaskList = new Task[taskList.length - 1];
+    // for (int i = 0; i < taskList.length; i++) {
+    // Task task = taskList[i];
+    // if (task.id() != id) {
+    // newTaskList[currentIndex] = task;
+    // currentIndex++;
+    // }
+    // }
+    // taskList = newTaskList;
+    // return taskList;
 
-    @DeleteMapping("/tasks/{id}")
-    public Task[] removeTask(@PathVariable("id") Long id) {
-        int currentIndex = 0;
-        Task[] newTaskList = new Task[taskList.length - 1];
-        for (int i = 0; i < taskList.length; i++) {
-            Task task = taskList[i];
-            if (task.id() != id) {
-                System.out.println("TASK ID");
-                System.out.println(task.id());
-                newTaskList[currentIndex] = task;
-                currentIndex++;
-            }
-        }
-        taskList = newTaskList;
-        return taskList;
-
-    }
+    // }
 }
