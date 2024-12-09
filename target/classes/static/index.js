befedcd70e330ce3e2f9e46c9872d191ef2e3297
BuildTools <ssnazy767@gmail.com>
2024-12-09 13:48:12 +0100

    added stuff, the commit is unfinished

--- target/classes/static/index.js
+++ target/classes/static/index.js
@@ -1,16 +1,31 @@
 const taskContainer = document.getElementById("TaskContainer")
+const listBackground = document.getElementById("Background")
 const taskTemplate = document.getElementById("taskTemplate")
 const taskCounter = document.getElementById("taskCounter")
+const addButton = document.getElementById("AddButton")
+const creatorTemplate = document.getElementById("creatorTemplate")
 const colors = ["#180161", "#4F1787","#EB3678","#FB773C"]
+let mode = "none"
 
 let currentColor = 0
 
 function generateTaskColor() {
     if (currentColor > colors.length - 1) currentColor = 0
-    const color = colors[currentColor]
+    const color = colors[currentColor]  
     currentColor += 1
     return color
 }
+
+
+function enterTaskCreator(element) {
+    mode = "taskCreator"
+    console.log(creatorTemplate)
+    const creator = creatorTemplate.cloneNode(true)
+    listBackground.classList.add("taskFormBlur")
+    document.body.appendChild(creator)
+}
+
+addButton.addEventListener("click", enterTaskCreator);
 
 function addTask(title = "None",description = "None",date = "None") {
     const color = generateTaskColor()
