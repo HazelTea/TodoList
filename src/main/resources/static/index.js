70a5205a578423c2cd3a40bc68678e974b7fff67
BuildTools <ssnazy767@gmail.com>
2024-12-10 20:03:01 +0100

    Removed some interactive animations due to the lag they caused somehow. I'll add them back once the api is somewhat functional.

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -37,23 +37,26 @@ function reactToMouse(event,main,interaction) {
     const gradient = background.children["mouseGradient"]
     const rect = background.getBoundingClientRect()
     const x = event.clientX - (rect.left + rect.width / 2)
-    const y = event.clientY - (rect.top + rect.height / 2)
+    const y = event.clientY - (rect.top + rect.height / 2) 
 
-    if (interaction=="moving" && !startingAnim) {
-        gradient.style.display = "block" 
-        background.style.perspective = "500px"
-        background.style.transform = `rotateX(${-y / 10}deg) rotateY(${x / 10}deg)`
-        background.style.transition = "transform 0s"
-        const gradientRect = gradient.getBoundingClientRect()
-        const x2 = event.clientX - (rect.left + gradientRect.width / 2)
-        const y2 = event.clientY - (rect.top + gradientRect.height / 2)
-        gradient.style.left = x2 + "px"
-        gradient.style.top = y2 + "px"
+    switch (interaction) {
+        case "moving":
+            // background.style.transform = `rotateX(${-y / 7.5}deg) rotateY(${x / 7.5}deg)`
+            if (gradient.style.display != "block") gradient.style.display = "block" 
+            // if (background.style.transition != "transform 0.1s") background.style.transition = "transform 0.1s"
 
-    } else if (interaction=="leaving") {
-        gradient.style.display = "none" 
-        background.style.transition = "transform 0.5s"
-        background.style.transform = `rotateX(0deg) rotateY(0deg)`
+            const gradientRect = gradient.getBoundingClientRect()
+            const x2 = event.clientX - (rect.left + gradientRect.width / 2)
+            const y2 = event.clientY - (rect.top + gradientRect.height / 2)
+            gradient.style.left = x2 + "px"
+            gradient.style.top = y2 + "px"
+            break
+
+        case "leaving":
+            gradient.style.display = "none" 
+            // background.style.transition = "transform 0.5s"
+            // background.style.transform = `rotateX(0deg) rotateY(0deg)`
+            break
     }
 
 }
@@ -88,8 +91,8 @@ function enterTaskCreator() {
 async function taskCreatorSave(saveButton) { 
     const form = saveButton.parentNode
 
-    const title = form.title.value == "" ? "None" : form.title.value 
-    const desc = form.desc.value == "" ? "None" : form.desc.value 
+    const title = form.title.value ? "NONE" : form.title.value.toUpperCase()
+    const desc = form.desc.value ? "None" : form.desc.value 
     const year = form.year.value || "1970"
     const month = form.month.value || "01"
     const day = form.day.value || "01"
@@ -110,24 +113,27 @@ function leaveTaskCreator(button) {
     mode = "none"
 }
 
-addButton.addEventListener("click", enterTaskCreator);
-
-function addTaskUI(title = "None",description = "None",date = "None") {
+function addTaskUI(task) {
     const color = generateTaskColor()
     const clonedTemplate = taskTemplate.content.cloneNode(true)
     const frame = clonedTemplate.children["frame"]
+    frame.id = `task_${task.id}`
     const background = frame.children["background"]
     const titleElement = background.children["title"]
     const descElement = background.children["description"]
     const dateElement = background.children["deadline"]
-    titleElement.innerHTML = title
-    descElement.innerHTML = description
-    dateElement.innerHTML = date
+    titleElement.innerHTML = task.title 
+    descElement.innerHTML = task.description
+    const date = new Date(task.deadline)
+    dateElement.innerHTML = formatDate(date)
     dateElement.style.fontSize = "10px"
 
     background.style.backgroundColor = color
     background.style.backgroundImage = `linear-gradient(-150deg, ${color} 50%,rgb(1,1,1,50%))`
     taskContainer.appendChild(clonedTemplate)
+
+    frame.addEventListener("mousemove", (e) => {reactToMouse(e,frame,"moving")});
+    frame.addEventListener("mouseleave", (e) => {reactToMouse(e,frame,"leaving")});
 }
 
 async function getTasks() {
@@ -142,19 +148,17 @@ async function saveTask(title,desc,deadline) {
 
 async function updateTaskCount() {
     const tasks = await getTasks()
-    console.log(tasks.length)
     taskCounter.innerHTML = tasks.length || "0"
 }
 
 async function updateTaskList() {
     const tasks = await getTasks()
     tasks.forEach(task => {
-        const title = !task.title ? "None" : task.title
-        const desc = !task.description ? "None" : task.description
-        const date = new Date(task.deadline)
-        addTaskUI(title,desc,formatDate(date))
+        addTaskUI(task)
     });
 }
+
+addButton.addEventListener("click", enterTaskCreator);
 
 updateTaskList()
 updateTaskCount()
