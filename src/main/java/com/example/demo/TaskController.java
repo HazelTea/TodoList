6a93435c36ccb240736e512c84d0d0fdf1e65269
Hazel <ssnazy767@gmail.com>
2025-02-13 20:07:47 +0100

    Improved the code readability.

--- src/main/java/com/example/demo/TaskController.java
+++ src/main/java/com/example/demo/TaskController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -70,26 +69,9 @@ public class TaskController {
     public @ResponseBody Task editTask(@PathVariable("id") Integer id,
             @RequestParam Map<String, String> params) {
         Task task = getTaskById(id);
-        String[] keyArray = params.keySet().toArray(String[]::new);
-        String[] valueArray = params.values().toArray(String[]::new);
-        for (int i = 0; i < params.size(); i++) {
-            switch (keyArray[i]) {
-                case "title":
-                    System.out.println("ballllll " + valueArray[i]);
-                    task.setTitle(valueArray[i]);
-                    break;
-                case "description":
-                    task.setDescription(valueArray[i]);
-                    break;
-                case "deadline":
-                    task.setDeadline(valueArray[i]);
-                    break;
-
-                default:
-                    System.err.println("Invalid attribute.");
-                    break;
-            }
-        }
+        task.setTitle(params.get("title"));
+        task.setDescription(params.get("description"));
+        task.setDeadline(params.get("deadline"));
         taskList.save(task);
         return task;
     }
